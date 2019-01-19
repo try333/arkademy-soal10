257e3283ae0499bfa553c69e1cb49cc4e1c21c72
try333 <triumpar@gmail.com>
2019-01-19 23:28:34 +0800

    Upload file

--- 4.php
+++ 4.php
@@ -2,10 +2,13 @@
 
 function print_abc($kolom,$baris){
 $abjad=array('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','v','q','r','s','t','u','v','w','x','y','z');
+$ix=0;
 for($h=0;$h<$baris;$h++){
 	for($i=0;$i<$kolom;$i++)
-
+	
 		echo $abjad[$i];
+		echo $abjad[$ix];
+		$ix++;
 echo "<br>";
 }
 }
